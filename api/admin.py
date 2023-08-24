608b6d8cf210f987897f8bd479368c95235ddaa1
Mohamed Sarhan <mohsarhan88@gmail.com>
2023-08-24 22:27:45 +0300

    refactor(db): refactor models and add models functions

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
+from .models import *
 # Register your models here.
+
+UserAdmin.fieldsets +=(
+  ('Extra Fields', {'fields': ('type','balance' )}),
+)
+
+class LoanAdmin(admin.ModelAdmin):
+  list_display =['borrower','total_loan_amount','loan_amount_after_annual_interest_rate','loan_amount','loan_period','lenme_fee','loan_status']
+  readonly_fields=('loan_amount_after_annual_interest_rate',)
+  
+class ScheduledPaymentsAdmin(admin.ModelAdmin):
+  list_display=['borrower','investor','payment_amount','payment_date','is_last_payment']
+admin.site.register(CustomUser,UserAdmin)
+admin.site.register(Loan,LoanAdmin)
+admin.site.register(Offer)
+admin.site.register(ScheduledPayments,ScheduledPaymentsAdmin)
--- api/models.py
+++ api/models.py
@@ -1,51 +1,76 @@
 from datetime import timedelta,date
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from enum import Enum
+import datetime
+from dateutil.relativedelta import relativedelta
+from decimal import Decimal
 # Create your models here.
 
-class UserType(Enum):
-  BORRWOER = "BORRWOER"
-  INVESTOR = "INVESTOR"
-
-class LoanStatus(Enum):
-  FUNDED = "FUNDED"
-  PENDING = "PENDING"
-  COMPLETED ="COMPLETED"
-  
 class CustomUser(AbstractUser):
+  class UserType(models.TextChoices):
+      BORRWOER = "BORRWOER"
+      INVESTOR = "INVESTOR"
   type = models.CharField(max_length=250,choices=UserType.choices)
   balance = models.DecimalField(default=0.00,max_digits=10,decimal_places=2)
   
-# class Borrower(models.Model):
-#     user = models.OneToOneField(CustomUser,on_delete=models.CASCADE,related_name="type")
-
-# class Investor(models.Model):
-#     user = models.OneToOneField(CustomUser,on_delete=models.CASCADE,related_name="type")
 
 class Loan(models.Model):
-    borrower = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    investor = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+  
+    class LoanStatus(models.TextChoices):
+        FUNDED = "FUNDED"
+        PENDING = "PENDING"
+        COMPLETED ="COMPLETED"
+        
+    borrower = models.ForeignKey(CustomUser, on_delete=models.CASCADE,related_name="borrower_loans")
     loan_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    loan_amount_after_annual_interest_rate= models.DecimalField(max_digits=10, decimal_places=2,default=0.00)
     loan_period = models.PositiveIntegerField(help_text="in months")
-    annual_interest_rate = models.DecimalField(max_digits=5, decimal_places=2)
     loan_status = models.CharField(max_length=20, choices=LoanStatus.choices, default=LoanStatus.PENDING)
     lenme_fee=models.DecimalField(max_digits=10, decimal_places=2, default=3.00)
-    loan_endtime = models.DateField(default=(date.today() + timedelta(weeks=(loan_period*4))))
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
     
     @property
     def total_loan_amount(self):
-      return self.loan_amount+self.lenme_fee
-      
+      # pass
+      return self.loan_amount_after_annual_interest_rate+self.lenme_fee
+    
+    
     def __str__(self):
       return self.borrower.username
+    
+    def calc_loan_amount_after_annual_interest_rate(self,rate):
+      self.loan_amount_after_annual_interest_rate = self.loan_amount+(self.loan_amount*(rate/100))
+      self.save()
+      
+    def create_scheduled_payments(self,investor):
+      amount=self.total_loan_amount/self.loan_period
+      months=self.get_next_n_months_dates_from_next_month(self.loan_period)
+      
+      scheduled_payments = []
+      for index,date in enumerate(months):
+        scheduled_payment = ScheduledPayments(
+          borrower=self.borrower,
+          investor=investor,
+          payment_amount=amount,
+          payment_date=date,
+          loan=self
+          )
+        if index == len(months)-1:
+          scheduled_payment.is_last_payment=True
+        scheduled_payments.append(scheduled_payment)
+      ScheduledPayments.objects.bulk_create(scheduled_payments)
+    
+    def get_next_n_months_dates_from_next_month(self,n):
+      today = datetime.date.today()
+      next_month = today + relativedelta(months=1)
+      next_n_months_dates = [next_month + relativedelta(months=i) for i in range(n)]
+      return next_n_months_dates
     
 class Offer(models.Model):
   investor=models.ForeignKey(CustomUser,on_delete=models.CASCADE)
   loan=models.ForeignKey(Loan, on_delete=models.CASCADE)
-  annual_interest_rate=models.DecimalField(max_digits=10,decimal_places=2,default=0.00)
+  annual_interest_rate=models.DecimalField(max_digits=10,decimal_places=2,default=0.00,help_text="out of 100% ")
   is_accepted=models.BooleanField(default=False)
   created_at=models.DateTimeField(auto_now_add=True)
   updated_at=models.DateTimeField(auto_now=True)
@@ -53,11 +78,35 @@ class Offer(models.Model):
   def __str__(self) -> str:
     return self.investor.username
   
-class ScheduedPayments(models.Model):
-  borrower=models.ForeignKey(CustomUser, on_delete=models.Case)
+  def save(self, *args, **kwargs):
+    """Create scheduled payments and recalculate the loan total amount if offer accepted"""
+    if self.is_accepted:
+      
+      self.loan.loan_status = "FUNDED"
+      self.loan.calc_loan_amount_after_annual_interest_rate(self.annual_interest_rate)
+      self.loan.save()
+      
+      self.loan.create_scheduled_payments(self.investor)
+      self.add_amount_investor_to_borrower()
+      super().save(*args, **kwargs)
+        
+    else:
+      super().save(*args, **kwargs)
+    
+
+  
+  def add_amount_investor_to_borrower(self,):
+    self.investor.balance-=self.loan.loan_amount
+    self.loan.borrower.balance+=self.loan.loan_amount
+    self.investor.save()
+    self.loan.borrower.save()
+  
+class ScheduledPayments(models.Model):
+  borrower=models.ForeignKey(CustomUser, on_delete=models.Case,related_name="borrower_payments")
   investor=models.ForeignKey(CustomUser, on_delete=models.Case)
+  loan=models.ForeignKey(Loan, on_delete=models.CASCADE)
   payment_amount=models.DecimalField(max_digits=10,decimal_places=2)
-  payment_data=models.DateField()
+  payment_date=models.DateField()
   is_paid = models.BooleanField(default=False)
   is_last_payment = models.BooleanField(default=False)
   created_at=models.DateTimeField(auto_now_add=True)
